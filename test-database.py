08e30282632c8648c445211818526f60ab187935
Abhiram Shibu <abhiramshibu1998@gmail.com>
2016-11-15 14:24:40 +0530

    Python 3 ify!

--- test-database.py
+++ test-database.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 import os
 a=os.popen("ls databases")
 if a.read()=="":
-   print "Database error Occured!"
+   print("Database error Occured!")
 else :
-  print "Database found!"
-print "End of database detection"
+  print("Database found!")
+print("End of database detection")
