9bdf3ed2fbffe444d1d3a25bc037ae8b5dd56366
ABHIRAM SHIBU <abhiramshibu1998@gmail.com>
2016-12-16 11:41:26 +0530

    Deleted rubbish

--- source/HelloWorld.cpp
+++
@@ -1,8 +0,0 @@
-//Just a Hello World program to look at a compiled program
-#include<iostream>  // To use io
-int main()
-{
-	std::cout<<"Hello World"<<std::endl;    // calling std class in iostream and using cout.
-	return 0;                     // What ever, i like to return some value, it happens to make me happy
-}
-// "HelloWorld" will be the binary object code of this code
--- source/antivirus.py
+++ source/antivirus.py
@@ -3,34 +3,7 @@
 #convert binary to hex
 import glob                # useless, for developer purpose
 import binascii            # u know it , hex - dec - ascii - bin
-def scan_abandoned(f1,f2):       # abandoned no need to know
-	found=0
-	count=0
-	for i in range(len(f1)):
-		temp=i	
-		k=1		
-		if found==1:
-			break
-		else :
-			count=0
-		for j in range(len(f2)) :
-			if f1[i]==j:
-				count+=1
-				if k in range(20):
-					if f1[temp+k]==f2[j+k]:
-						count+=1
-					if count==20:
-						found=1
-						print "Infected Be scared! BOOM!"
-#				if i<len(f1):
-#					i+=1
-				print count
-			else:
-				count=0
-			if count==20:
-				print "Infected Be scared! BOOM!"
-				found=1
-				break
+
 def scan(f1,f2):                          # New can definition
 	found=0                           # flag as found
 	count=0                           # To keep track of counter
@@ -42,6 +15,10 @@ def scan(f1,f2):                          # New can definition
 		print "WoW Infected!."    # alert user
 	else :                            # if not infected
 		print "Im disappointed its free from virus." # you know it
+
+
+
+
 def split(a,b):                           # Split function, string -> list
 	f1=[]                             # Initialize empty variable as a list
 	f2=[]                             # Initialze again 
@@ -52,6 +29,8 @@ def split(a,b):                           # Split function, string -> list
 	#print "F1 \n",f1
 	#print "F2 \n",f2
 	return f1,f2
+
+
 
 def binhex():
 	fvirus=open("databases","rb").readline()#open virus database sample
@@ -81,16 +60,11 @@ def filter(f1,f2):
 
 
 
-import io
-f=io.FileIO("cleancode.txt","w")
-
-
-
-
-
-neat=filter(f1,f2)
+#import io
+#f=io.FileIO("cleancode.txt","w")
+#neat=filter(f1,f2)
 #f.open('cleancode.py','w+')
-f.write(binascii.unhexlify(neat))
-f.close()		
+#f.write(binascii.unhexlify(neat))
+#f.close()		
 
 
